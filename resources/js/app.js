219e3d772bda8a6fb3ca6ed0ae75355c4f3f2c20
Muhammad Eiman Iqrar <muhammadeimaniqrar786@gmail.com>
2024-07-27 15:58:17 +0500

    Vue JS Integrated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import { createApp } from "vue";
+import app from "./layout/app.vue";
+
+createApp(app).mount("#app");
---
+++ resources/js/layout/app.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Assalam u Alaikum</h1>
+    </div>
+</template>
